08b34570eb2a2405f1ad520da917e3118a6ab260
Psor1107 <psor1107@gmail.com>
2024-04-15 23:44:46 -0300

    Organização em Campos e Registros
    Faça um programa que leia o arquivo compras.txt e calcule o total da compra. Esse
    arquivo contém o nome, a quantidade e o preço de diversos produtos. No início de
    cada registro existe um indicador de tamanho de 1 byte. Esse indicador fornece o
    tamanho total, em bytes, do registro a ser lido. Os campos são separados
    internamente por delimitadores do tipo “|”.
    Um exemplo do arquivo é mostrado a seguir.

---
+++ Prática03/main.c
@@ -0,0 +1,50 @@
+/*
+Gabriel Lucchetta Garcia Sanchez - 828513
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *arquivo;
+    char buffer[100];
+    int quantidade = 0;
+    float preco = 0.0;
+    float total = 0.0;
+    int tamanho = 0;
+
+    int i = 0;
+
+    arquivo = fopen("compras.txt", "r");
+    if (arquivo == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }
+    while (1)
+    {
+        tamanho = fgetc(arquivo);
+
+        if (tamanho == EOF)
+        {
+            break;
+        };
+
+        fread(buffer, sizeof(char), tamanho, arquivo);
+        buffer[tamanho] = '\0';
+        for (i = 0; i < tamanho; i++)
+        {
+            if (buffer[i] == '|')
+            {
+                quantidade = atoi(&buffer[i + 1]);
+                preco = atof(&buffer[i + 3]);
+                break;
+            };
+        };
+        total += quantidade * preco;
+    }
+    fclose(arquivo);
+    printf("Total da compra: R$%.2f\n", total);
+
+    return 0;
+}
