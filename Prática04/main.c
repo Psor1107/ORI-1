09eb3f078568dd52388eb54939f4faa91864f1a0
Gabriel <82649581+Psor1107@users.noreply.github.com>
2024-04-23 13:08:34 -0300

    delete nodes

--- Prática04/main.c
+++ Prática04/main.c
@@ -18,14 +18,24 @@ struct node
     struct node *next;
 } typedef Node;
 
-void addNode(Node **head, Compras *compra);
+struct DeletedNode
+{
+    int RRN;
+    struct DeletedNode *next;
+} typedef DeletedNode;
+
+int removeNode(Node **head, DeletedNode *free, int RRN);
+void addNode(Node **head, DeletedNode *free, Compras *compra);
 
 int main()
 {
     FILE *arquivo;
     Compras buffer;
     Node *node = NULL;
+    DeletedNode *deletedNode = NULL;
     float total = 0.0;
+
+    //removeNode(&node, deletedNode, 0);
 
     arquivo = fopen("compras.bin", "rb");
     if (arquivo == NULL)
@@ -33,30 +43,45 @@ int main()
         printf("Erro ao abrir o arquivo\n");
         return 1;
     }
-    
+
     while (1) 
     {
         fread(&buffer.ativo, sizeof(char), 1, arquivo);
-        if (feof(arquivo)) 
+        if(feof(arquivo)){break;} 
+        if (buffer.ativo != '0') 
         {
-            break;
-        }
-        fread(&buffer.nome, sizeof(char), 51, arquivo);
-        fread(&buffer.qtd, sizeof(int), 1, arquivo);
-        fread(&buffer.valor, sizeof(float), 1, arquivo);
-        total += buffer.qtd * buffer.valor;
-        addNode(&node, &buffer);
-
+            fread(&buffer.nome, sizeof(char), 51, arquivo);
+            fread(&buffer.qtd, sizeof(int), 1, arquivo);
+            fread(&buffer.valor, sizeof(float), 1, arquivo);
+            total += buffer.qtd * buffer.valor;
+            addNode(&node, deletedNode, &buffer);
+        }else{
+            fseek(arquivo, sizeof(Compras)-sizeof(char), SEEK_CUR);
+        }    
     }
     fclose(arquivo);
     printf("Total da compra: R$%.2f\n", total);
     return 0;
 }
 
-void addNode(Node **head, Compras *compra)
+void addNode(Node **head, DeletedNode *free, Compras *compra)
 {
-    Node *newNode = (Node *)malloc(sizeof(Node));
-    newNode->compra = *compra;
-    newNode->next = *head;
-    *head = newNode;
+    if(free == NULL){
+        Node *newNode = (Node *)malloc(sizeof(Node));
+        newNode->compra = *compra;
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }else{
+        
+    }
+}
+
+int removeNode(Node **head, DeletedNode *free, int RRN)
+{
+    FILE *arquivo = fopen("compras.bin", "rb+");
+    fseek(arquivo, (RRN*sizeof(Compras)), SEEK_SET);
+    fwrite("0", sizeof(char), 1, arquivo);
+    fclose(arquivo);
+    return 0;
 }
