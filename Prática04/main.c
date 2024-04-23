c438dd0b7a6811da818bea31b7a8cb565ba70aa3
Psor1107 <psor1107@gmail.com>
2024-04-23 19:30:14 -0300

    addEntry removeEntry added

--- Prática04/main.c
+++ Prática04/main.c
@@ -14,36 +14,99 @@ struct compras
 
 struct node
 {
-    Compras compra;
+    int RRN;
     struct node *next;
 } typedef Node;
 
-struct DeletedNode
-{
-    int RRN;
-    struct DeletedNode *next;
-} typedef DeletedNode;
+int removeEntry(Node **free, int i, FILE *arquivo);
+int addEntry(Node **free_node, FILE *arquivo, Compras *data);
 
-int removeNode(Node **head, DeletedNode *free, int RRN);
-void addNode(Node **head, DeletedNode *free, Compras *compra);
+void printDeletedNode(Node *free);
+void printFile();
+
+float getTotal(FILE *arquivo);
 
 int main()
 {
-    FILE *arquivo;
-    Compras buffer;
-    Node *node = NULL;
-    DeletedNode *deletedNode = NULL;
+    FILE *arquivo = NULL;
+    Node *deletedNode = NULL;
     float total = 0.0;
 
-    //removeNode(&node, deletedNode, 0);
+    //Caso de teste
+    
+    //addEntry(&deletedNode, arquivo, &(Compras){'1', "macarrao", 2, 5.0});
+    //removeEntry(&deletedNode, 2, arquivo);
+    //addEntry(&deletedNode, arquivo, &(Compras){'1', "desinfetante", 6, 4.0});
 
-    arquivo = fopen("compras.bin", "rb");
+    //Prints de arquivo
+    
+    printFile();
+    printDeletedNode(deletedNode);
+    
+    total = getTotal(arquivo);
+    printf("Total da compra: R$%.2f\n", total);
+    
+    return 0;
+}
+
+int addEntry(Node **free_node, FILE *arquivo, Compras *data)
+{
+    arquivo = fopen("compras.bin", "rb+");
     if (arquivo == NULL)
     {
         printf("Erro ao abrir o arquivo\n");
         return 1;
     }
+    if(*free_node == NULL){
+        fseek(arquivo, 0, SEEK_END);
+        fwrite(&data->ativo, sizeof(char), 1, arquivo);
+        fwrite(&data->nome, sizeof(char), 51, arquivo);
+        fwrite(&data->qtd, sizeof(int), 1, arquivo);
+        fwrite(&data->valor, sizeof(float), 1, arquivo);
+    }else{
+        Node *aux = *free_node;
+        fseek(arquivo, (aux->RRN*sizeof(Compras)), SEEK_SET);
+        fwrite(&data->ativo, sizeof(char), 1, arquivo);
+        fwrite(&data->nome, sizeof(char), 51, arquivo);
+        fwrite(&data->qtd, sizeof(int), 1, arquivo);
+        fwrite(&data->valor, sizeof(float), 1, arquivo);
+        *free_node = aux->next;
+        free(aux);
+    }
+    fclose(arquivo);
+    return 0;
+}
 
+int removeEntry(Node **free, int i, FILE *arquivo)
+{
+    arquivo = fopen("compras.bin", "rb+");
+    if (arquivo == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }
+    fseek(arquivo, (i*sizeof(Compras)), SEEK_SET);
+    fwrite("0", sizeof(char), 1, arquivo);
+    
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->RRN = i;
+    newNode->next = *free;
+    *free = newNode;
+    fclose(arquivo);
+    return 0;
+}
+
+float getTotal(FILE *arquivo)
+{
+    
+    Compras buffer;
+    float total = 0.0;
+    arquivo = fopen("compras.bin", "rb+");
+    if (arquivo == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }
     while (1) 
     {
         fread(&buffer.ativo, sizeof(char), 1, arquivo);
@@ -54,34 +117,47 @@ int main()
             fread(&buffer.qtd, sizeof(int), 1, arquivo);
             fread(&buffer.valor, sizeof(float), 1, arquivo);
             total += buffer.qtd * buffer.valor;
-            addNode(&node, deletedNode, &buffer);
         }else{
-            fseek(arquivo, sizeof(Compras)-sizeof(char), SEEK_CUR);
+            fseek(arquivo, sizeof(Compras) - sizeof(char), SEEK_CUR);
         }    
     }
     fclose(arquivo);
-    printf("Total da compra: R$%.2f\n", total);
-    return 0;
+    return total;
 }
 
-void addNode(Node **head, DeletedNode *free, Compras *compra)
+void printFile()
 {
-    if(free == NULL){
-        Node *newNode = (Node *)malloc(sizeof(Node));
-        newNode->compra = *compra;
-        newNode->next = *head;
-        *head = newNode;
-        return;
-    }else{
-        
+    Compras buffer;
+    FILE *arquivo = fopen("compras.bin", "rb");
+    fseek(arquivo, 0, SEEK_SET);
+    while (1) 
+    {
+        fread(&buffer.ativo, sizeof(char), 1, arquivo);
+        if(feof(arquivo)){break;} 
+        if (buffer.ativo != '0') 
+        {
+            fread(&buffer.nome, sizeof(char), 51, arquivo);
+            fread(&buffer.qtd, sizeof(int), 1, arquivo);
+            fread(&buffer.valor, sizeof(float), 1, arquivo);
+            //print buffer
+            printf("Nome: %s\n", buffer.nome);
+            printf("Quantidade: %d\n", buffer.qtd);
+            printf("Valor: %.2f\n\n", buffer.valor);
+        }else{
+            fseek(arquivo, sizeof(Compras) - sizeof(char), SEEK_CUR);
+        }  
     }
+    fclose(arquivo);
 }
 
-int removeNode(Node **head, DeletedNode *free, int RRN)
+void printDeletedNode(Node *free)
 {
-    FILE *arquivo = fopen("compras.bin", "rb+");
-    fseek(arquivo, (RRN*sizeof(Compras)), SEEK_SET);
-    fwrite("0", sizeof(char), 1, arquivo);
-    fclose(arquivo);
-    return 0;
+    Node *aux = free;
+    printf("RRN Limpos: ");
+    while (aux != NULL)
+    {
+        printf("%d", aux->RRN);
+        aux = aux->next;
+    }
+    printf("\n");
 }
