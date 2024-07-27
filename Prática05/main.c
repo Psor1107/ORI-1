6fbb27d53e8c435411f1f62ee0f2b2eaa5c7dedb
Gabriel <82649581+Psor1107@users.noreply.github.com>
2024-07-27 15:41:21 -0300

    Index File Sort

--- Prática05/main.c
+++ Prática05/main.c
@@ -1,8 +1,12 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct escola{
+#define BASE 8192
+
+/*
+struct escola
+{
     int id;
     char estado[5];
     char municipio[50];
@@ -12,50 +16,102 @@ struct escola{
     float media_linguagem;
     float media_matematica;
     float media_redacao;
-}typedef Escola;
+} typedef Escola;
+*/
 
-struct indicePrimario{
+struct indicePrimario
+{
     int id;
     long posicao;
-}typedef IndicePrimario;
+} typedef IndicePrimario;
 
-struct indiceSecundario{
+struct indiceSecundario
+{
     char estado[5];
     long posicao;
-}typedef IndiceSecundario;
+} typedef IndiceSecundario;
 
-void criaIndices (FILE *dados, FILE *dataIndicePrimario, FILE *dataIndiceSecundario){
+int sortPrimario(const void *a, const void *b)
+{
+    return ((IndicePrimario *)a)->id - ((IndicePrimario *)b)->id;
+}
+
+int sortSecundario(const void *a, const void *b)
+{
+    return strcmp(((IndiceSecundario *)a)->estado, ((IndiceSecundario *)b)->estado);
+}
+
+int criaIndices(FILE *dados, FILE *dataIndicePrimario, FILE *dataIndiceSecundario)
+{
 
     char linha[250];
     long posicao = 0;
-    
-    IndicePrimario indicePrimario;
-    IndiceSecundario indiceSecundario;
+    int i = 0;
+    int capacidade = BASE;
 
-    while (1) {
-        
-        posicao = ftell(dados);
+    IndicePrimario *indicePrimario = (IndicePrimario *)malloc(capacidade * sizeof(IndicePrimario));
+    IndiceSecundario *indiceSecundario = (IndiceSecundario *)malloc(capacidade * sizeof(IndiceSecundario));
 
-        if(fgets(linha, 250, dados) == NULL){
-            break;
+    if (indicePrimario == NULL || indiceSecundario == NULL)
+    {
+        printf("Erro ao alocar memoria!\n");
+        return 1;
+    }
+
+    while (fgets(linha, 250, dados) != NULL)
+    {
+        if (i >= capacidade)
+        {
+
+            capacidade *= 2;
+            indicePrimario = (IndicePrimario *)realloc(indicePrimario, (capacidade * sizeof(IndicePrimario)));
+            indiceSecundario = (IndiceSecundario *)realloc(indiceSecundario, (capacidade * sizeof(IndiceSecundario)));
+
+            if (indicePrimario == NULL || indiceSecundario == NULL)
+            {
+                printf("Erro ao alocar memoria!\n");
+                return 1;
+            }
         }
 
-        indicePrimario.posicao = posicao;
-        indiceSecundario.posicao = posicao;
+        posicao = ftell(dados);
+
+        indicePrimario[i].posicao = posicao;
+        indiceSecundario[i].posicao = posicao;
 
         char *token = strtok(linha, ";"); // id
 
-        indicePrimario.id = atoi(token);
-        fwrite(&indicePrimario, sizeof(IndicePrimario), 1, dataIndicePrimario);
+        indicePrimario[i].id = atoi(token);
+        // fwrite(&indicePrimario, sizeof(IndicePrimario), 1, dataIndicePrimario);
 
         token = strtok(NULL, ";"); // estado
 
-        strncpy(indiceSecundario.estado, token, sizeof(indiceSecundario.estado));
-        fwrite(&indiceSecundario, sizeof(IndiceSecundario), 1, dataIndiceSecundario);
+        strncpy(indiceSecundario[i].estado, token, sizeof(indiceSecundario[i].estado));
+        // fwrite(&indiceSecundario, sizeof(IndiceSecundario), 1, dataIndiceSecundario);
+
+        i++;
     }
+
+    qsort(indicePrimario, i, sizeof(IndicePrimario), sortPrimario);
+    qsort(indiceSecundario, i, sizeof(IndiceSecundario), sortSecundario);
+
+    for (int c = 0; c < i; c++)
+    {
+        fwrite(&indicePrimario[c], sizeof(IndicePrimario), 1, dataIndicePrimario);
+        fwrite(&indiceSecundario[c], sizeof(IndiceSecundario), 1, dataIndiceSecundario);
+    }
+
+    free(indicePrimario);
+    free(indiceSecundario);
+
+    return 0;
 }
 
-int main(){
+int main()
+{
+
+    int output = 0;
+
     FILE *dados = fopen("DadosEnem.txt", "r");
     if (dados == NULL)
     {
@@ -74,9 +130,18 @@ int main(){
         printf("Erro no arquivo secundario\n");
         return 1;
     }
-    criaIndices(dados, dataIndicePrimario, dataIndiceSecundario);
+
+    if(criaIndices(dados, dataIndicePrimario, dataIndiceSecundario)){
+        printf("Erro ao criar os indices\n");
+        output = 1;
+    }else{
+        printf("Indices criados com sucesso!\n");
+        output = 0;
+    }
+
     fclose(dados);
     fclose(dataIndicePrimario);
     fclose(dataIndiceSecundario);
-    return 0;
+
+    return output;
 }
